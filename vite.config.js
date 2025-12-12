a4fd3e4a8f396df46432176e1d184ba1ac3d684b
norpulatovv <yoldoshevxasan98@gmail.com>
2025-12-12 16:07:17 +0500

    the end

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My-practise-2/"
+  base: process.env.NODE_ENV === 'production' ? '/My-practise-2/' : '/'
 })
